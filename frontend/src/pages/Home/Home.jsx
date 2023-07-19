1bdd1e1ee8ab3cbb625f8361a4448275ebac85b2
jerome vilanova <jerome.vilanova@gmail.com>
2023-07-19 17:03:26 +0200

    fix

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <p>content</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+// import { Link } from "react-router-dom";
+import "./Login.scss";
+
+export default function Login() {
+  return (
+    <div>
+      <p>component</p>
+    </div>
+  );
+}
