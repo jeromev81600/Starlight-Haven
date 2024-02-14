332f15cacc6f8dcd9cc96b8320308ad3d4dda000
jerome vilanova <jerome.vilanova@gmail.com>
2024-02-14 18:49:06 +0100

    ajout fenêtres accordéons sur équipements

--- frontend/src/components/EquipmentCard/EquipmentCard.jsx
+++ frontend/src/components/EquipmentCard/EquipmentCard.jsx
@@ -1,14 +1,49 @@
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./EquipmentCard.scss";
-import { useState } from "react";
 
 export default function EquipmentCard({ backpackData, equipments }) {
-  const [titleHovered, setTitleHovered] = useState(false);
-  console.info("hover", titleHovered);
+  // Utilise un tableau d'états pour garder une trace de l'état de chaque équipement
+  const [sectionHovered, setSectionHovered] = useState(
+    Array(equipments.length).fill(false)
+  );
+
+  // Fonction qui permet de basculer l'état d'un équipement au survol, repasse tous les autres à false lorsqu'on passe un élément à true (évite les bugs)
+  const toggleSection = (index) => {
+    const newSectionHovered = Array(equipments.length).fill(false);
+    newSectionHovered[index] = true;
+    setSectionHovered(newSectionHovered);
+  };
+
+  // Fonction qui permet de repasser l'état d'un élément à false au click lorsque celui-ci est ouvert
+  const closeSection = (index) => {
+    const newSectionHovered = [...sectionHovered];
+    newSectionHovered[index] = false;
+    setSectionHovered(newSectionHovered);
+  };
+
+  // Fonction qui permet de repasser l'état de tous les éléments à false en cliquant à côté.
+  const resetSections = () => {
+    setSectionHovered(Array(equipments.length).fill(false));
+  };
+
+  // Gestionnaire d'événement clavier
+  const handleKeyDown = (event) => {
+    // Si la touche "Escape" est pressée, réinitialiser les sections
+    if (event.key === "Escape") {
+      resetSections();
+    }
+  };
 
   return (
     backpackData && (
-      <div className="EquipmentCardContainer">
+      <div
+        role="button"
+        tabIndex={0}
+        className="EquipmentCardContainer"
+        onClick={resetSections}
+        onKeyDown={handleKeyDown}
+      >
         <div className="EquipmentBackpackCard">
           <div className="EquipmentBackpackCardTitleBox">
             <h2>{backpackData.type}</h2>
@@ -39,31 +74,40 @@ export default function EquipmentCard({ backpackData, equipments }) {
           </div>
         </div>
         <div className="EquipmentCards">
-          {equipments.map((equipment) => (
-            <div className="EquipmentCard" key={equipment.id}>
-              <div
-                className="EquipmentCardTitleBox"
-                onMouseEnter={() => setTitleHovered(true)}
-                onMouseLeave={() => setTitleHovered(false)}
-              >
+          {equipments.map((equipment, index) => (
+            <div
+              role="button"
+              tabIndex={-1}
+              className="EquipmentCard"
+              key={equipment.id}
+              onClick={() => closeSection(index)}
+              onMouseEnter={() => toggleSection(index)}
+              onMouseLeave={() => toggleSection(index)}
+              onKeyDown={handleKeyDown}
+            >
+              <div className="EquipmentCardTitleBox">
                 <h2>{equipment.equipmentname}</h2>
               </div>
-              {titleHovered && (
-                <div className="EquipmentCardContentBox">
-                  <p>
-                    <h3>Type :</h3> {equipment.type}
-                  </p>
-                  <p>
-                    <h3>Caractéristiques :</h3> {equipment.description}
-                  </p>
-                  <p>
-                    <h3>Volume :</h3> entre {equipment.volumem} litres
-                  </p>
-                  <p>
-                    <h3>Poids :</h3> {equipment.weight} Kg
-                  </p>
-                </div>
-              )}
+              <div
+                className={`EquipmentCardContentBox ${
+                  sectionHovered[index]
+                    ? "CardContent-show"
+                    : "CardContent-hide"
+                }`}
+              >
+                <p>
+                  <h3>Type :</h3> {equipment.type}
+                </p>
+                <p>
+                  <h3>Caractéristiques :</h3> {equipment.description}
+                </p>
+                <p>
+                  <h3>Volume :</h3> entre {equipment.volumem} litres
+                </p>
+                <p>
+                  <h3>Poids :</h3> {equipment.weight} Kg
+                </p>
+              </div>
             </div>
           ))}
         </div>
